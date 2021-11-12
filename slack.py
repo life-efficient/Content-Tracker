4794a5dd950d06903c5a657611dde32e7e9875a5
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-11-12 15:45:22 +0000

    Ignore PRs in the message

--- slack.py
+++ slack.py
@@ -14,6 +14,7 @@ def check_len(issue, this_week):
         message += f'{len(issue)} issues were opened {message_time}:\n'
         for i in issue:
             message += f"<{i.html_url}|{i.html_url.split('/')[-1]}> \t"
+        message += '\n'
     else:
         message += f'No issues were opened {message_time}\n'
 
@@ -36,9 +37,11 @@ channel_id = "C02GMMQUQ56"  # Content Operation
 # channel_id = "C02MBCYLF08" # Test Channel
 current = []
 late = []
+today = datetime.now()
 
-for n, issue in enumerate(g.get_user().get_repo(repo).get_issues()):
-    today = datetime.now()
+for n, issue in enumerate(g.get_user().get_repo(repo).get_issues(state='open')):
+    if issue.pull_request:
+        continue
     diff = (today - issue.created_at).days
     if diff > 7:
         late.append(issue)
