34e33dbf4d02c524aedc2babaf18e010c1a4b2fb
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-11-12 15:36:40 +0000

    Update messages

--- slack.py
+++ slack.py
@@ -4,6 +4,28 @@ from github import Github
 from datetime import datetime
 from slack_sdk import WebClient
 
+def check_len(issue, this_week):
+    if this_week:
+        message_time = 'this week'
+    else:
+        message_time = 'more than one week ago'
+    message = ''
+    if len(issue) > 0:
+        message += f'{len(issue)} issues were opened {message_time}:\n'
+        for i in issue:
+            message += f"<{i.html_url}|{i.html_url.split('/')[-1]}> \t"
+    else:
+        message += f'No issues were opened {message_time}\n'
+
+    return message
+
+def create_messages(n, repo, current, late):
+    message = f'There are {n} issue(s) in {repo} \n'
+    message += check_len(current, True)
+    message += check_len(late, False)
+    return message
+
+
 slack_token = sys.argv[1]
 git_token = sys.argv[2]
 repo = sys.argv[3]
@@ -12,13 +34,21 @@ client = WebClient(slack_token)
 g = Github(git_token)
 channel_id = "C02GMMQUQ56"  # Content Operation
 # channel_id = "C02MBCYLF08" # Test Channel
-message = f'Issues in {repo}: \n'
+current = []
+late = []
 
-for issue in g.get_user().get_repo(repo).get_issues():
+for n, issue in enumerate(g.get_user().get_repo(repo).get_issues()):
     today = datetime.now()
     diff = (today - issue.created_at).days
-    message += ' ' + issue.title + ' : \t' + str(diff) + ' days ago' + '\n'
-    message += ' ' + issue.html_url + '\n\n'
+    if diff > 7:
+        late.append(issue)
+    else:
+        current.append(issue)
+
+if n > 0:
+    message = create_messages(n, repo, current, late)
+else:
+    message = f'No issues found in {repo}'
 
 result = client.chat_postMessage(
                     channel=channel_id,
