f958b851461fcef51099255caaa2934a193795cf
Ivan Ying <ying.yingxuan@hotmail.com>
2021-12-31 06:16:59 +0100

    Implements bugs in the messages

--- slack.py
+++ slack.py
@@ -5,33 +5,46 @@ from datetime import datetime
 from datetime import timedelta
 from slack_sdk import WebClient
 
-def check_len(issue, this_week, week_ago):
+def check_len(issue_bugs, issue_non_bugs, this_week, week_ago):
     if this_week:
         message_time = 'this week'
-        query = f'?q=is%3Aissue+is%3Aopen+created%3A%3E{week_ago}+'
+        query_non_bug = f'?q=is%3Aopen+is%3Aopen+created%3A%3D%3E{week_ago}+-label%3Abug+'
+        query_bug = f'?q=is%3Aopen+is%3Aopen+created%3A%3D%3E{week_ago}+label%3Abug+'
         emoji = ':warning:'
+        emoji_bug = ':bug:'
     else:
         message_time = 'more than one week ago'
-        query = f'?q=is%3Aissue+is%3Aopen+created%3A%3C{week_ago}'
+        query_non_bug = f'?q=is%3Aopen+is%3Aopen+created%3A%3C{week_ago}+-label%3Abug+'
+        query_bug = f'?q=is%3Aopen+is%3Aopen+created%3A%3C{week_ago}+label%3Abug+'
         emoji = ':rotating_light:'
+        emoji_bug = ':ladybug:'
         
     message = ''
-    if len(issue) > 1:
-        issues_link = '/'.join(issue[0].html_url.split('/')[:-1])
-        message += f"        {emoji} {len(issue)} <{issues_link}{query}|issues> were opened {message_time}\n"
-    elif len(issue) == 1:
-        issues_link = '/'.join(issue[0].html_url.split('/')[:-1])
-        message += f"        {emoji} Just one <{issues_link}{query}|issue> was opened {message_time}. Almost there!\n"
-    elif len(issue) == 0 and not this_week:
+    if len(issue_non_bugs) > 1:
+        issues_link = '/'.join(issue_non_bugs[0].html_url.split('/')[:-1])
+        message += f"        {emoji} {len(issue_non_bugs)} <{issues_link}{query_non_bug} |non-bug issues> were opened {message_time}\n"
+    elif len(issue_non_bugs) == 1:
+        issues_link = '/'.join(issue_non_bugs[0].html_url.split('/')[:-1])
+        message += f"        {emoji} Just one <{issues_link}{query_non_bug} |non-bug issue> was opened {message_time}. Almost there!\n"
+    elif len(issue_non_bugs) == 0 and not this_week:
         pass
-    else:
-        message += f'        :white_check_mark: No issues were opened {message_time}\n'
+
+    if len(issue_bugs) > 1:
+        issues_link = '/'.join(issue_bugs[0].html_url.split('/')[:-1])
+        message += f"        {emoji_bug} {len(issue_bugs)} <{issues_link}{query_bug}|bug issues> were opened {message_time}\n"
+    elif len(issue_bugs) == 1:
+        issues_link = '/'.join(issue_bugs[0].html_url.split('/')[:-1])
+        message += f"        {emoji_bug} Just one <{issues_link}{query_bug}|bug issue> was opened {message_time}. Almost there!\n"
+    elif len(issue_bugs) == 0 and not this_week:
+        pass
+    elif len(issue_bugs) == 0 and len(issue_non_bugs) == 0:
+        message += f'        :white_check_mark: No issues this week. Bravo!\n'
 
     return message
 
-def create_messages(current, late, week_ago):
-    message = check_len(current, True, week_ago)
-    message += check_len(late, False, week_ago)
+def create_messages_issue(current_bugs, current_non_bugs, late_bugs, late_non_bugs, week_ago):
+    message = check_len(current_bugs, current_non_bugs, True, week_ago)
+    message += check_len(late_bugs, late_non_bugs, False, week_ago)
     return message
 
 def create_messages_pr(prs, repo, n):
@@ -55,8 +68,10 @@ client = WebClient(slack_token)
 g = Github(git_token)
 channel_id = "C02GMMQUQ56"  # Content Operation
 # channel_id = "C02MBCYLF08" # Test Channel
-current = []
-late = []
+current_nonbugs = []
+current_bugs = []
+late_nonbugs = []
+late_bugs = []
 today = datetime.now()
 week_ago = datetime.strftime(today - timedelta(days=7), '%Y-%m-%d')
 prs = []
@@ -67,12 +82,21 @@ for n, issue in enumerate(g.get_user().get_repo(repo).get_issues(state='open')):
         continue
     diff = (today - issue.created_at).days
     if diff > 7:
-        late.append(issue)
+        if 'bug' in [label.name for label in issue.labels]:
+            late_bugs.append(issue)
+        else:
+            late_nonbugs.append(issue)
     else:
-        current.append(issue)
+        if 'bug' in [label.name for label in issue.labels]:
+            current_bugs.append(issue)
+        else:
+            current_nonbugs.append(issue)
 
 # Check issues without considering PRs
-n = len(current) + len(late)
+n = len(current_nonbugs) + \
+    len(current_bugs) + \
+    len(late_nonbugs) + \
+    len(late_bugs)
 
 # Initialize message with the PRs
 message = ''
@@ -84,17 +108,17 @@ if len(prs) > 0:
 if n > 1:
     if len(prs) > 0:
         message += f'and {n} issues\n'
-        message += create_messages(current, late, week_ago)
+        message += create_messages_issue(current_bugs, current_nonbugs, late_bugs, late_nonbugs, week_ago)
     else:
         message += f'*{repo}* has {n} issues\n'
-        message += create_messages(current, late, week_ago)
+        message += create_messages_issue(current_bugs, current_nonbugs, late_bugs, late_nonbugs, week_ago)
 elif n == 1:
     if len(prs) > 0:
         message += f'and just one issue\n'
-        message += create_messages(current, late, week_ago)
+        message += create_messages_issue(current_bugs, current_nonbugs, late_bugs, late_nonbugs, week_ago)
     else:
         message += f'*{repo}* has just one issue \n'
-        message += create_messages(current, late, week_ago) 
+        message += create_messages_issue(current_bugs, current_nonbugs, late_bugs, late_nonbugs, week_ago) 
 
 if len(prs) + n > 0:
     result = client.chat_postMessage(
